5da6126ae6cdb80117923a8085c2bbb6340f56cd
EmelianovVitaly <vitalik.emelianov@gmail.com>
2015-04-02 10:42:10 +0300

    deleted

--- cell_annotations/getAllComments.js
+++
@@ -1,9 +0,0 @@
-var getAllComments = (function(cell) {
-
-        if (cell != null) {
-            return cell.metadata.comments;
-        } else {
-            throw "getAllComments: Invalid cell!"
-        }
-    }
-);
